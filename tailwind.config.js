d0f201fc41b0ea7b1c856d75826e5cb7c05b1700
Donya <thisis2nya@gmail.com>
2022-09-16 20:45:39 +0430

    Subscribe section created

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
         servicesLeftDots: 'url("/img/services-left-dots.png")',
         servicesRightDots: 'url("/img/services-right-dots.png")',
         plansLeftVector: 'url("/img/plan-left-dec.png")',
+        subscribeBg: 'url("/img/subscribe-bg.png")',
+        subscribeVector: 'url("/img/subscribe-dec.png")',
+      },
+      backgroundImage: {
+        subscribeBg: 'url("/img/subscribe-bg.png")',
       },
     },
   },
