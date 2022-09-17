c1a5c03ac5183ded009abbd7b8c0110c1075ec6b
Donya <thisis2nya@gmail.com>
2022-09-17 12:13:37 +0430

    Footer created

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,7 @@ module.exports = {
         servicesLeftDots: 'url("/img/services-left-dots.png")',
         servicesRightDots: 'url("/img/services-right-dots.png")',
         plansLeftVector: 'url("/img/plan-left-dec.png")',
-        subscribeBg: 'url("/img/subscribe-bg.png")',
         subscribeVector: 'url("/img/subscribe-dec.png")',
-      },
-      backgroundImage: {
-        subscribeBg: 'url("/img/subscribe-bg.png")',
       },
     },
   },
